e760a6961ef0dc1c41161247b9b3e200db0a457e
intern2020 <phyozinmar1312000@gmail.com>
2020-10-04 14:53:52 +0630

    my angular firsr app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './login/login.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './test4/test4.component';
+import { Test5Component } from './test5/test5.component';
+import { Test6Component } from './test6/test6.component';
+import { Test7Component } from './test7/test7.component';
+import { Test8Component } from './test8/test8.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    LoginComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+    Test5Component,
+    Test6Component,
+    Test7Component,
+    Test8Component
   ],
   imports: [
     BrowserModule,
